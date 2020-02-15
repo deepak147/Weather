dbbd7dd15b63834f68792baa270b12b7a273acfb
deepak147 <sluvvy@gmail.com>
2020-02-16 01:27:05 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -20,14 +20,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Dk'
+        name: 'Deepak'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Dk'
+        name: 'Deepak'
     })
 })
 
@@ -35,7 +35,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Dk'
+        name: 'Deepak'
     })
 })
 
@@ -81,7 +81,7 @@ app.get('/products', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Dk',
+        name: 'Deepak',
         errorMessage: 'Help article not found.'
     })
 })
@@ -89,7 +89,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Dk',
+        name: 'Deepak',
         errorMessage: 'Page not found.'
     })
 })
